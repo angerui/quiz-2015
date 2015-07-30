c1c4e114e66d62e7e4a8443c5a9fd705f5e23711
angerui <angerui@yahoo.es>
2015-07-30 13:41:14 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* Página de entrada (GET home page). */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res, next) {
 
 // Autoload de comandos con :quizId:
 router.param('quizId', quizController.load); //autoload :quizId
+
+// Definición de rutas de sesión
+router.get('/login', sessionController.new);        // formulario login
+router.post('/login', sessionController.create);    // crear sesión
+router.get('/logout', sessionController.destroy);   // destruir sesión
 
 router.get('/author', function(req, res) {
   res.render('author', { autor: 'Angel Ramón Ruiz Pajares', errors: [] });
