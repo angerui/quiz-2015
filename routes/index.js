78566a77d23f28e5406d5aaa989deeec9b1783ae
angerui <angerui@yahoo.es>
2015-07-31 10:58:26 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/', function(req, res, next) {
 });
 
 // Autoload de comandos con :quizId:
-router.param('quizId', quizController.load); //autoload :quizId
+router.param('quizId', quizController.load);        //autoload :quizId
+router.param('commentId', commentController.load);  //autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new);        // formulario login
@@ -32,7 +33,9 @@ router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizCo
 router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
+//Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired,  commentController.publish);  //Debería usarse put para seguir el estándar REST
 
 module.exports = router;
